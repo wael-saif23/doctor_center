9dbfef7b8fb40fce5aed2e414f9b04521cbf04dc
Wael Saif <ws.mywork@gmail.com>
2024-12-21 20:09:53 +0200

    UI Login Screen
    ** in core **
    - create spacing file helper
    - add some colors and font style
    - create font weight helper file
    - create app text button widget
    - create app form field widget
    ** in features **
    - create some sub text widget
    - UI login screen

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -1,9 +1,10 @@
 import 'package:doctor_center/core/theming/colors.dart';
+import 'package:doctor_center/core/theming/font_weight_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TextStyles {
-  static TextStyle fontSize32Bold = TextStyle(
+  static TextStyle fontSize32BlueBold = TextStyle(
       color: ColorsManager.mainBlue,
       fontSize: 32.sp,
       fontWeight: FontWeight.bold);
@@ -13,8 +14,73 @@ class TextStyles {
       fontSize: 12.sp,
       fontWeight: FontWeight.w400);
 
+  static TextStyle font13GrayRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.normal,
+    color: ColorsManager.mainGrey,
+  );
+
   static TextStyle fontSize12semiBoldWhite = TextStyle(
       color: ColorsManager.mainWhite,
-      fontSize: 16.sp,
+      fontSize: 12.sp,
       fontWeight: FontWeight.w600);
+
+  static TextStyle font16WhiteMedium = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeightHelper.medium,
+    color: Colors.white,
+  );
+
+  static TextStyle font13BlueSemiBold = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.semiBold,
+    color: ColorsManager.mainBlue,
+  );
+  static TextStyle font13DarkBlueMedium = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.medium,
+    color: ColorsManager.darkBlue,
+  );
+  static TextStyle font13DarkBlueRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: ColorsManager.darkBlue,
+  );
+  static TextStyle font24BlueBold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeightHelper.bold,
+    color: ColorsManager.mainBlue,
+  );
+  static TextStyle font16WhiteSemiBold = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeightHelper.semiBold,
+    color: Colors.white,
+  );
+
+  static TextStyle font13BlueRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: ColorsManager.mainBlue,
+  );
+  static TextStyle font14GrayRegular = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: ColorsManager.mainGrey,
+  );
+  static TextStyle font14LightGrayRegular = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: ColorsManager.lightGray,
+  );
+  static TextStyle font14DarkBlueMedium = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.medium,
+    color: ColorsManager.darkBlue,
+  );
+
+  static TextStyle font24BlackBold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeightHelper.bold,
+    color: Colors.black,
+  );
 }
--- lib/features/login/login_screen.dart
+++ lib/features/login/login_screen.dart
@@ -1,14 +1,92 @@
+import 'package:doctor_center/core/helpers/spacing.dart';
+import 'package:doctor_center/core/theming/styles.dart';
+import 'package:doctor_center/core/widgets/app_text_button.dart';
+import 'package:doctor_center/core/widgets/app_text_form_field.dart';
+import 'package:doctor_center/features/login/ui/widgets/already_have_account_text.dart';
+import 'package:doctor_center/features/login/ui/widgets/terms_and_conditions_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  bool isObscureText = true;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('LoginScreen'),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Welcome Back',
+                    style: TextStyles.font24BlueBold,
+                  ),
+                  getVerticalSpacing(8),
+                  Text(
+                    'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                    style: TextStyles.font14GrayRegular,
+                  ),
+                  getVerticalSpacing(36),
+                  Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        const AppTextFormField(hintText: 'Email'),
+                        getVerticalSpacing(18),
+                        AppTextFormField(
+                          hintText: 'Password',
+                          isObscureText: isObscureText,
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                isObscureText = !isObscureText;
+                              });
+                            },
+                            child: Icon(
+                              isObscureText
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                            ),
+                          ),
+                        ),
+                        getVerticalSpacing(24),
+                        Align(
+                          alignment: AlignmentDirectional.centerEnd,
+                          child: Text(
+                            'Forgot Password?',
+                            style: TextStyles.font13BlueRegular,
+                          ),
+                        ),
+                        getVerticalSpacing(40),
+                        AppTextButton(
+                          buttonText: "Login",
+                          textStyle: TextStyles.font16WhiteSemiBold,
+                          onPressed: () {},
+                        ),
+                        getVerticalSpacing(16),
+                        const TermsAndConditionsText(),
+                        getVerticalSpacing(60),
+                        const AlreadyHaveAccountText(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
